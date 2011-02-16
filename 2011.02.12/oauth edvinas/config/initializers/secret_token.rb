50e4865259845efd99bf92725373e60ec0b84cd9
Edvinas Bartkus <edvinas@edvinas-VirtualBox.(none)>
2011-02-16 15:43:54 +0200

    OAuth-plugin fail by Edvinas

---
+++ 2011.02.12/oauth edvinas/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandboxOauth::Application.config.secret_token = '6af1f8b026e358816cf1a0b6ed864b4aed774de12b1d6be1fcf8ca70223b847ce26dbb09552092600bcd87d92a900d6e553e6efed57a73170834f6ec76f9cc82'
